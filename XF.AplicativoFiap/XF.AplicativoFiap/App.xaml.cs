f8cd970dde9690bcc5f43dd68b4032a66322faa0
Vinícius <vinicius.cs01@gmail.com>
2017-07-22 16:53:17 -0300

    Criação do Projeto

---
+++ XF.AplicativoFiap/XF.AplicativoFiap/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+using XF.AplicativoFiap.ViewModel;
+
+namespace XF.AplicativoFiap
+{
+    public partial class App : Application
+    {
+        public static ProfessorViewModel ProfessorVM { get; set; }
+
+        public App()
+        {
+            InitializeComponent();
+            InitializeApplication();
+
+            MainPage = new NavigationPage(new View.Professor.ProfessorListView { BindingContext = ProfessorVM });
+        }
+
+
+        private void InitializeApplication()
+        {
+            if (ProfessorVM == null) ProfessorVM = new ProfessorViewModel();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ XF.AplicativoFiap/XF.AplicativoFiap/ViewModel/ProfessorViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using XF.AplicativoFiap.Model;
+
+namespace XF.AplicativoFiap.ViewModel
+{
+    public class ProfessorViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Professor> Professores { get; set; } = new ObservableCollection<Professor>();
+        public List<Professor> ListProfessores = new List<Professor>();
+
+        private Professor selecionado;        
+        public Professor Selecionado
+        {
+            get { return selecionado; }
+            set
+            {
+                selecionado = value as Professor;
+                EventPropertyChanged();
+            }
+        }
+
+        public ICommand OnDeleteProfessorCMD { get; private set; }
+        public ICommand OnAdicionarProfessorCMD { get; private set; }
+        public ICommand OnNovoCMD { get; private set; }
+        public ICommand OnEditProfessorCMD { get; private set; }
+
+        public ProfessorViewModel()
+        {
+            OnDeleteProfessorCMD = new Command<Professor>(OnDelete);
+            OnAdicionarProfessorCMD = new Command<Professor>(OnAdicionar);
+            OnNovoCMD = new Command(OnNovo);
+            OnEditProfessorCMD = new Command<Professor>(OnEdit);
+            Carregar();
+        }
+
+        private async void Carregar()
+        {            
+            ListProfessores = await ProfessorRepository.GetProfessoresSqlAzureAsync();
+            AplicaFiltro();
+        }
+
+        void OnDelete(Professor professor)
+        {
+            
+        }
+
+        private void OnAdicionar(Professor professor)
+        {
+            
+        }
+
+        private async void OnNovo()
+        {
+            App.ProfessorVM.Selecionado = new Professor();
+            await Application.Current.MainPage.Navigation.PushAsync
+                (new View.Professor.NovoProfessorView { BindingContext = App.ProfessorVM });
+        }
+
+        private async void OnEdit(Professor professor)
+        {
+            App.ProfessorVM.Selecionado = professor;
+
+            await Application.Current.MainPage.Navigation.PushAsync
+                (new View.Professor.NovoProfessorView { BindingContext = App.ProfessorVM });
+        }
+
+        private void AplicaFiltro()
+        {
+            for (int i = 0; i < ListProfessores.Count; i++)
+            {
+                Professor professor = ListProfessores[i];
+                Professores.Insert(i, professor);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void EventPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
